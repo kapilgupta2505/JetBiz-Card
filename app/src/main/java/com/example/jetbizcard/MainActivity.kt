b339b01f5cfd04e23225097290d4664032de31e3
kapil gupta <kapil.k.0501@gmail.com>
2024-11-29 18:01:39 +0530

    New Project created  and made some changes

---
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.jetbizcard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetbizcard.ui.theme.JetBizCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetBizCardTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    CreateBizCard()
+                }
+                }
+            }
+        }
+    }
+
+@Composable
+fun CreateBizCard() {
+    Surface( modifier = Modifier.fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier.width(200.dp)
+            .height(390.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp) {
+
+        }
+
+    }
+
+
+}
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetBizCardTheme {
+        CreateBizCard()
+    }
+}
