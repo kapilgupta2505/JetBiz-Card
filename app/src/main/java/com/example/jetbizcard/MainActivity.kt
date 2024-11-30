30d513b80d9ef4adb164e40d67144a7af7c3671e
kapil gupta <kapil.k.0501@gmail.com>
2024-11-30 09:09:35 +0530

    New Project created  and made some changes

--- app/src/main/java/com/example/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/example/jetbizcard/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.jetbizcard
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -14,11 +12,10 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetbizcard.ui.theme.JetBizCardTheme
@@ -42,8 +39,10 @@ fun CreateBizCard() {
     Surface( modifier = Modifier.fillMaxWidth()
         .fillMaxHeight()) {
         Card(modifier = Modifier.width(200.dp)
-            .height(390.dp),
+            .height(390.dp)
+            .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            backgroundColor = Color.Green,
             elevation = 4.dp) {
 
         }
